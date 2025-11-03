22dad2d5e4a3476a49e4a1142a9e62f32e0c31bc
Eduards Mendez <55601494+eguar11011@users.noreply.github.com>
2025-11-03 15:43:42 -0500

    notebooks with visualizacion and experiment sum

---
+++ sum_int_64.jl
@@ -0,0 +1,23 @@
+function add_int64(loop::Int)
+    data = Int64[
+        158, 21, 7813, 632, 87, 14, 751, 201, 79, 26,
+        158_862, 78_213, 425_763, 412_489, 852_362,
+        23_546, 145_823, 352_689, 558_721
+    ]
+
+    println("Addition Computation (Int64 - unified set)")
+    println("=========================================")
+
+    acc = Int64(0)
+
+    for k in 1:loop
+        for a in data, b in data
+            acc += a + b
+        end
+    end
+
+    println("Resultado acumulado (acc) = $acc")
+end
+
+# Ejecutar cálculo
+add_int64(1_000_000)
